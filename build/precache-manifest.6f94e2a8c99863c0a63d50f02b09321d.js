f2f895bdcb2c0df8fc2c914db0589d10b96c122a
villatrue <scarter0313@gmail.com>
2020-01-29 23:32:56 -0500

    removed build from gitignore

---
+++ build/precache-manifest.6f94e2a8c99863c0a63d50f02b09321d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bdc085f83f89df24f214e18a7304bfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27323b18361af8e3c7bf",
+    "url": "/static/css/main.eb601df5.chunk.css"
+  },
+  {
+    "revision": "f01e2bac785c9ddf08f0",
+    "url": "/static/js/2.84a99434.chunk.js"
+  },
+  {
+    "revision": "e5fdd3c10a39c54f9723efcb1214ee6a",
+    "url": "/static/js/2.84a99434.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27323b18361af8e3c7bf",
+    "url": "/static/js/main.1aaa88ff.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
